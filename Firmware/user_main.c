82ce9e271c2789a09a3d7336cb1341defa35aae2
Shrinath Nimare <shrinath.nimare@gmail.com>
2018-08-11 20:24:14 +0530

    Improved maintainability

--- Firmware/user_main.c
+++ Firmware/user_main.c
@@ -38,12 +38,30 @@ initFunction()
 void ICACHE_FLASH_ATTR
 itwerk()
 {
-    OledStringStruct p;
-    OledStringPtr pt[7];
+    OledStringStruct *p = (OledStringStruct *)os_zalloc(sizeof(OledStringStruct));
+    OledStringPtr *pt = (OledStringPtr*)os_zalloc(7);
     OledFunctions_stringToOledString("itworks", pt);
-    p.ptr = pt;
-    p.len = 7;
-    OledFunctions_writeString(&p);
+    p->ptr = pt;
+    p->len = 7;
+    OledFunctions_eraseScreen();
+    OledFunctions_returnCursor();
+    OledFunctions_writeString(p);
+    os_free(p);
+    os_free(pt);
+}
+
+void ICACHE_FLASH_ATTR
+timeFunction()
+{
+    OledStringStruct * temp = (OledStringStruct *)os_zalloc(sizeof(OledStringStruct));
+    uint8 length = (uint8)os_strlen(sntp_get_real_time(realTime));
+    OledStringPtr * pt = (OledStringPtr *)os_zalloc(length);
+    OledFunctions_stringToOledString(sntp_get_real_time(realTime), pt);
+    temp->len = length;
+    temp->ptr = pt;
+    OledFunctions_writeString(temp);
+    os_free(temp);
+    os_free(pt);
 }
 
 //==========ENTRY POINT==========
@@ -51,16 +69,16 @@ void user_init(void)
 {
     system_uart_swap();
     initFunction();
-    static OledStringPtr spa[6] = {8,19,4,12,42,27};
-    static OledStringPtr spb[6] = {8,19,4,12,42,28};
+    static OledStringPtr spa[13] = {0x0f, 0x11, 0x08, 0x0d, 0x13, 0x2a, 0x08, 0x13, 0x16, 0x0e, 0x11, 0x0a, 0x12};
+    static OledStringPtr spb[20] = {0x03, 0x08, 0x12, 0x0f, 0x0b, 0x00, 0x18, 0x2a, 0x02, 0x14, 0x11, 0x11, 0x04, 0x0d, 0x13, 0x2a, 0x13, 0x08, 0x0c, 0x04};
     static OledStringPtr spc[6] = {8,19,4,12,42,29};
     static OledStringStruct a;
     static OledStringStruct b;
     static OledStringStruct c;
     a.ptr = spa;
-    a.len = 6;
+    a.len = 13;
     b.ptr = spb;
-    b.len = 6;
+    b.len = 20;
     c.ptr = spc;
     c.len = 6;
     MenuStruct *menuStruct;
@@ -68,7 +86,7 @@ void user_init(void)
     menuStruct->totalElements = 3;
     menuStruct->currentElement = 2;
     OledFunctions_optionSet(menuStruct, 0, itwerk, &a);
-    OledFunctions_optionSet(menuStruct, 1, NULL, &b);
+    OledFunctions_optionSet(menuStruct, 1, timeFunction, &b);
     OledFunctions_optionSet(menuStruct, 2, NULL, &c);
     OledFunctions_setGlobalMenu(menuStruct);
 }
